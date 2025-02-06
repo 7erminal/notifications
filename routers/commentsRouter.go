a568a517eb2691f5d0bb6821f31ba8082579cb55
Bede Abbe <bede.abbe@gmail.com>
2025-02-06 12:18:55 +0000

    changes

---
+++ routers/commentsRouter.go
@@ -0,0 +1,55 @@
+package routers
+
+import (
+	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["notification_service/controllers:NotificationsController"] = append(beego.GlobalControllerRouter["notification_service/controllers:NotificationsController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["notification_service/controllers:NotificationsController"] = append(beego.GlobalControllerRouter["notification_service/controllers:NotificationsController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["notification_service/controllers:NotificationsController"] = append(beego.GlobalControllerRouter["notification_service/controllers:NotificationsController"],
+        beego.ControllerComments{
+            Method: "GetOne",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["notification_service/controllers:NotificationsController"] = append(beego.GlobalControllerRouter["notification_service/controllers:NotificationsController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["notification_service/controllers:NotificationsController"] = append(beego.GlobalControllerRouter["notification_service/controllers:NotificationsController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
